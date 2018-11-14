7dd607687244cc951ecce98bd4bf652661395da6
Spencer Henze <spencer.henze@allata.com>
2018-11-13 21:32:53 -0700

    merged in interceptors

---
+++ StackUnderflow/src/interceptors/error-redirect.interceptor.ts
@@ -0,0 +1,31 @@
+import { map, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+	HttpErrorResponse,
+	HttpEvent,
+	HttpHandler,
+	HttpInterceptor,
+	HttpRequest,
+	HttpResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+	public initialLoad = true;
+	constructor(private router: Router
+	) {
+	}
+
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		return next.handle(req).pipe(tap((event: HttpEvent<any>) => { }, (err: any) => {
+			if (err instanceof HttpErrorResponse) {
+				if (err.status === 401) {
+					this.router.navigate(['login']);
+				}
+			}
+		}));
+	}
+}
